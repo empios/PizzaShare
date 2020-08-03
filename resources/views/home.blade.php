720897f97c526950b781aac7b410195a8964f24d
EmpioS <pawel.wlo@wp.pl>
2020-08-03 15:46:30 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <h2>Avatar</h2>
+        </div>
+    </div>
+</div>
+@endsection
